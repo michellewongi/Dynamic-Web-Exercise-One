53beabeb73c72d92c031a59bc8787769e8b282fb
mw3981 <mw3981@nyu.edu>
2021-09-14 08:44:34 -0400

    Create container

---
+++ src/containers/article.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function article() {
+  const articleTitle = "Single Article";
+  return (
+    <div>
+      <h1>{articleTitle}</h1>
+    </div>
+  );
+}
+
+export default article;
---
+++ src/containers/articleListing.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function articleListing() {
+  const articlesTitle = "Articles";
+  return (
+    <div>
+      <h1>{articlesTitle}</h1>
+    </div>
+  );
+}
+
+export default articleListing;
