7bb5d80d3a61238c0299d4f3cd9eb51b7fc0d17f
mw3981 <mw3981@nyu.edu>
2021-09-20 11:16:10 -0400

    Updating layout

--- src/containers/articleListing.js
+++ src/containers/articleListing.js
@@ -1,10 +1,17 @@
 import React from "react";
+import ArticleCard from "../components/ArticleCard";
+import Data from "../components/data.json";
 
 function ArticleListing() {
+  console.log(Data);
   const articlesTitle = "Articles";
   return (
-    <div className="page-wrapper">
-      <h1 className="header-one">{articlesTitle}</h1>
+    <div className="PageWrapper">
+      <h2 className="HeaderOneStyle">{articlesTitle}</h2>
+
+      {Data.map((article, key) => (
+        <ArticleCard article={article} key={key} />
+      ))}
     </div>
   );
 }
